48ff805ff4accf31f47e51934996f1fc6b4ded3f
tle <tlemakuz@gmail.com>
2024-06-06 17:55:34 +0700

    feat:state message

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+
 function MessageBoard() {
+const [textMessage, setTextMessage] = useState([]);
+const [addText, setAddtext] = useState("");
+
+const setNewText = (event) => 
+  setAddtext(event.target.value);
+
+const addData = () => {
+  const newData = [...textMessage]
+  newData.push(addText);
+  setTextMessage(newData);
+  setAddtext("");
+};
+
+const deleteData = (dataIndex) => {
+  const newData = [...textMessage]
+  newData.splice(dataIndex, 1);
+  setTextMessage(newData);
+};
+
+
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,16 +32,21 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={addText}
+            onChange={setNewText}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addData}>Submit</button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
-      </div>
+          <ul>
+              {textMessage.map((textMes, index) => (
+              <li key={index}><div className="message">
+                <h1>{textMessage}</h1>
+                <button className="delete-button" onClick={() => deleteData()}>x</button></div>
+              </li>))} 
+             </ul>
+          </div>
     </div>
   );
 }
